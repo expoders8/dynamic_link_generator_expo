550f73c716dc9e04a97ee75f80ab986f771c87e9
dashu6101 <dashu.web@gmail.com>
2026-02-06 19:20:51 +0530

    Remove all

--- src/prisma/prisma.module.ts
+++
@@ -1,9 +0,0 @@
-import { Global, Module } from '@nestjs/common';
-import { PrismaService } from './prisma.service';
-
-@Global() // Optional: makes it available app-wide without needing to import everywhere
-@Module({
-  providers: [PrismaService],
-  exports: [PrismaService],
-})
-export class PrismaModule {}
